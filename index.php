3e53b1ec458325d7e5c56784177cfa248cce67eb
Joey <josefgriffiths@me.com>
2022-02-28 21:36:33 +0000

    Added nav bar to index.php

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 <html>
 <head>
     title="index";
+    <style>
+
+    </style>
 </head>
 <body>
-
+    <ul>
+        <li name="home"><a class="active" href="/index.php">Home</a></li>
+        <li name="tetris"><a href="/tetris.php">Play Tetris</a></li>
+        <li name="leaderboard"><a href="/leaderboard.php">Leaderboard</a></li>
+    </ul>
 </body>
 </html>
