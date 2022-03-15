f0f34112413917b8b4fa373cf2936d5d56b5670b
Joey <josefgriffiths@me.com>
2022-03-15 17:08:43 +0000

    Edit table styling

--- ecm1417_coursework/leaderboard.php
+++ ecm1417_coursework/leaderboard.php
@@ -55,7 +55,7 @@
         table, th, td {
             border: 1px solid black;
             border-spacing: 2px;
-            width: auto;
+            width: 75%;
         }
         td {
             text-align: center;
